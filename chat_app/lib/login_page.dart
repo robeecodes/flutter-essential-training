5c23f1ce5b7f5ed9d3c1538a3e35cff2316c7b40
Robin <97387960+robeecodes@users.noreply.github.com>
2023-12-16 22:05:08 +0000

    feat(lib/login_page.dart): style images

--- chat_app/lib/login_page.dart
+++ chat_app/lib/login_page.dart
@@ -36,7 +36,18 @@ class LoginPage extends StatelessWidget {
                     fontSize: 24),
               ),
             ),
-            Image.network('https://3009709.youcanlearnit.net/Alien_LIL_131338.png')
+            Image.network(
+                'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
+                height: 200),
+            Container(
+              height: 150,
+              width: 150,
+              padding: const EdgeInsets.all(10),
+              margin: const EdgeInsets.all(50),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(24), color: Colors.blue),
+              child: const FlutterLogo(),
+            )
           ],
         ));
   }
