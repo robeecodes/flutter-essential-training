60ae0856cc74cbcfdf7837dced3bfdb814ac688d
Robin <97387960+robeecodes@users.noreply.github.com>
2023-12-16 20:14:37 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter-essential-training
+ My projects created for the Flutter Essential Training course on LinkedIn Learning
---
+++ _0001_chat_app/android/app/src/main/kotlin/com/example/u_0001_chat_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.u_0001_chat_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
