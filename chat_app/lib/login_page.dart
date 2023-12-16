dcf1f39129ad1c2f15f95b5a9483ca6a1b484d37
Robin <97387960+robeecodes@users.noreply.github.com>
2023-12-16 21:59:37 +0000

    feat(lib/login_page.dart): add text and image placeholder

--- chat_app/lib/login_page.dart
+++ chat_app/lib/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -13,6 +14,30 @@ class LoginPage extends StatelessWidget {
             print('Click!');
           },
         ),
-        body: const Text('Hello!'));
+        body: Column(
+          children: [
+            Text(
+              'Let\'s sign you in!',
+              style: GoogleFonts.poppins(
+                textStyle: const TextStyle(
+                    color: Colors.black54,
+                    letterSpacing: .5,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30),
+              ),
+            ),
+            Text(
+              'Welcome back!\nYou\'ve been missed!',
+              style: GoogleFonts.poppins(
+                textStyle: const TextStyle(
+                    color: Colors.black38,
+                    letterSpacing: .5,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24),
+              ),
+            ),
+            Image.network('https://3009709.youcanlearnit.net/Alien_LIL_131338.png')
+          ],
+        ));
   }
 }
