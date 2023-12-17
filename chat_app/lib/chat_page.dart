0aaf624cc74778aa5d5d96967e59c2a63a548d29
Robin <97387960+robeecodes@users.noreply.github.com>
2023-12-17 14:54:23 +0000

    feat(lib/chat_page.dart-&&-lib/login_page.dart): adjust alignment of ui

--- chat_app/lib/chat_page.dart
+++ chat_app/lib/chat_page.dart
@@ -15,88 +15,97 @@ class ChatPage extends StatelessWidget {
       ),
       body: ListView(
         children: [
-          Container(
-            margin: const EdgeInsets.all(50),
-            padding: const EdgeInsets.all(24),
-            decoration: const BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular((12)),
-                    topRight: Radius.circular(12),
-                    bottomLeft: Radius.circular(12))),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  'Hi, this is your message',
-                  style: GoogleFonts.poppins(
-                    textStyle: const TextStyle(
-                        color: Colors.white70,
-                        letterSpacing: .5,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+              margin: const EdgeInsets.all(50),
+              padding: const EdgeInsets.all(24),
+              decoration: const BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular((12)),
+                      topRight: Radius.circular(12),
+                      bottomLeft: Radius.circular(12))),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'Hi, this is your message',
+                    style: GoogleFonts.poppins(
+                      textStyle: const TextStyle(
+                          color: Colors.white70,
+                          letterSpacing: .5,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600),
+                    ),
                   ),
-                ),
-                Image.network(
-                    'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
-                    height: 200)
-              ],
+                  Image.network(
+                      'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
+                      height: 200)
+                ],
+              ),
             ),
           ),
-          Container(
-            margin: const EdgeInsets.all(50),
-            padding: const EdgeInsets.all(24),
-            decoration: const BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular((12)),
-                    topRight: Radius.circular(12),
-                    bottomLeft: Radius.circular(12))),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  'Hi, this is your message',
-                  style: GoogleFonts.poppins(
-                    textStyle: const TextStyle(
-                        color: Colors.white70,
-                        letterSpacing: .5,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Container(
+              margin: const EdgeInsets.all(50),
+              padding: const EdgeInsets.all(24),
+              decoration: const BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular((12)),
+                      topRight: Radius.circular(12),
+                      bottomLeft: Radius.circular(12))),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'Hi, this is your message',
+                    style: GoogleFonts.poppins(
+                      textStyle: const TextStyle(
+                          color: Colors.white70,
+                          letterSpacing: .5,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600),
+                    ),
                   ),
-                ),
-                Image.network(
-                    'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
-                    height: 200)
-              ],
+                  Image.network(
+                      'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
+                      height: 200)
+                ],
+              ),
             ),
           ),
-          Container(
-            margin: const EdgeInsets.all(50),
-            padding: const EdgeInsets.all(24),
-            decoration: const BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular((12)),
-                    topRight: Radius.circular(12),
-                    bottomLeft: Radius.circular(12))),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  'Hi, this is your message',
-                  style: GoogleFonts.poppins(
-                    textStyle: const TextStyle(
-                        color: Colors.white70,
-                        letterSpacing: .5,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Container(
+              margin: const EdgeInsets.all(50),
+              padding: const EdgeInsets.all(24),
+              decoration: const BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular((12)),
+                      topRight: Radius.circular(12),
+                      bottomLeft: Radius.circular(12))),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'Hi, this is your message',
+                    style: GoogleFonts.poppins(
+                      textStyle: const TextStyle(
+                          color: Colors.white70,
+                          letterSpacing: .5,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600),
+                    ),
                   ),
-                ),
-                Image.network(
-                    'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
-                    height: 200)
-              ],
+                  Image.network(
+                      'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
+                      height: 200)
+                ],
+              ),
             ),
           ),
           Container(
@@ -105,21 +114,20 @@ class ChatPage extends StatelessWidget {
                 color: Colors.black,
                 borderRadius: BorderRadius.vertical(top: Radius.circular(20))),
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 IconButton(
                     onPressed: () {},
                     icon: const Icon(
                       Icons.add,
                       color: Colors.white70,
-                    )
-                ),
+                    )),
                 IconButton(
                     onPressed: () {},
                     icon: const Icon(
                       Icons.send,
                       color: Colors.white70,
-                    )
-                ),
+                    )),
               ],
             ),
           )
--- chat_app/lib/login_page.dart
+++ chat_app/lib/login_page.dart
@@ -7,48 +7,37 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        drawer: const Drawer(),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            print('Click!');
-          },
-        ),
-        body: Column(
-          children: [
-            Text(
-              'Let\'s sign you in!',
-              style: GoogleFonts.poppins(
-                textStyle: const TextStyle(
-                    color: Colors.black54,
-                    letterSpacing: .5,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30),
-              ),
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            'Let\'s sign you in!',
+            style: GoogleFonts.poppins(
+              textStyle: const TextStyle(
+                  color: Colors.black54,
+                  letterSpacing: .5,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,),
             ),
-            Text(
-              'Welcome back!\nYou\'ve been missed!',
-              style: GoogleFonts.poppins(
-                textStyle: const TextStyle(
-                    color: Colors.black38,
-                    letterSpacing: .5,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 24),
-              ),
+          ),
+          Text(
+            'Welcome back!\nYou\'ve been missed!',
+            textAlign: TextAlign.center,
+            style: GoogleFonts.poppins(
+              textStyle: const TextStyle(
+                  color: Colors.black38,
+                  letterSpacing: .5,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24),
             ),
-            Image.network(
-                'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
-                height: 200),
-            Container(
-              height: 150,
-              width: 150,
-              padding: const EdgeInsets.all(10),
-              margin: const EdgeInsets.all(50),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(24), color: Colors.blue),
-              child: const FlutterLogo(),
-            )
-          ],
-        ));
+          ),
+          Image.network(
+              'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
+              height: 200),
+        ],
+      ),
+    ));
   }
 }
